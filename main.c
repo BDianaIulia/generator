24a4451d5646eaefba9fa3e0f7eaf5d4343b99a3
BDianaIulia <32904058+BDianaIulia@users.noreply.github.com>
2018-06-04 17:38:54 +0300

    small tests

--- main.c
+++ main.c
@@ -10,9 +10,15 @@ int main()
 
     srand((unsigned) time(NULL) );
 
-    for( iterator = 0 ; iterator < 10 ; iterator++ )
+    for( iterator = 0 ; iterator < 5 ; iterator++ )
     {
-        no_of_stories = rand() % 60 + 20;
+        no_of_stories = rand() % 15 + 5;
+        fprintf( file_path, "%d " , no_of_stories );
+    }
+
+    for( iterator = 0 ; iterator < 5 ; iterator++ )
+    {
+        no_of_stories = rand() % 60 + 10;
         fprintf( file_path, "%d " , no_of_stories );
     }
 
