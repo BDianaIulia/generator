3d549f5dabb5354c612ec02431171ed4dece60ea
BDianaIulia <32904058+BDianaIulia@users.noreply.github.com>
2018-06-05 14:35:22 +0300

    ready

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
-#include <stdio.h>
+///\file main.c
+///\brief C implementation for generator of inputs.
+///
+/// Created by Diana Bleoanca.
+
+#include <stdio.h> //for rand()
 #include <stdlib.h>
-#include <time.h>
+#include <time.h> // for srand()
 
 int main()
 {
+    ///\fn int main()
+    ///\brief Main function.
+    ///This function writes on file "input" 10 random integers.
+    ///First 5 integers have values between 5 and 19.
+    ///Other ones have value between 10 and 69.
+
+
     FILE *file_path = fopen( "input" , "w" );
     int iterator;
     int no_of_stories;
